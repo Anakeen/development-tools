aaf2fd72806ef502faba247ad524e07d5547d1e8
Charles Bonnissent <charles.bonnissent@anakeen.com>
2014-09-14 10:31:10 +0200

    feat(info): Correct info generation
    Correct multiple Application

--- src/Dcp/DevTools/Template/InfoXml.php
+++ src/Dcp/DevTools/Template/InfoXml.php
@@ -12,10 +12,10 @@ class InfoXml extends Template {
         if (!isset($arguments["name"]) || !$this->checkLogicalName($arguments["name"])) {
             throw new Exception("You need to set the name of the module with a valid name " . $this->logicalNameRegExp);
         }
-        if (isset($arguments["application"]) && !$this->checkLogicalName($arguments["application"])) {
+        if (isset($arguments["application"])) {
             $application = explode(",", $arguments["application"]);
             foreach ($application as $currentApplication) {
-                if (isset($currentApplication) && !$this->checkLogicalName($currentApplication)) {
+                if (!$this->checkLogicalName($currentApplication)) {
                     throw new Exception("You need to set the name of the application with a valid name $currentApplication : " . $this->logicalNameRegExp);
                 }
             }
