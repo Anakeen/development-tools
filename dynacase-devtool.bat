987db90c3883e85b50e906386044f47af6090104
Jérôme Augé <jerome.auge@anakeen.com>
2014-10-02 08:04:10 +0200

    Renamed remaining "devtool.phar" to "dynacase-devtool.phar"

--- dynacase-devtool.bat
+++ dynacase-devtool.bat
@@ -5,6 +5,6 @@ SETLOCAL
 SET DEVTOOL_PATH=%~dp0
 SET DEVTOOL_PATH=%DEVTOOL_PATH:~0,-1%
 
-SET PATH=%PATH%;%DEVTOOL_PATH%\devtool-bundle;%DEVTOOL_PATH%\devtool-bundle\bin
+SET PATH=%PATH%;%DEVTOOL_PATH%\dynacase-devtool-bundle;%DEVTOOL_PATH%\dynacase-devtool-bundle\bin
 
-"%DEVTOOL_PATH%\devtool-bundle\php.exe" "%DEVTOOL_PATH%\devtool-bundle\dynacase-devtool.phar" %*
+"%DEVTOOL_PATH%\dynacase-devtool-bundle\php.exe" "%DEVTOOL_PATH%\dynacase-devtool-bundle\dynacase-devtool.phar" %*
