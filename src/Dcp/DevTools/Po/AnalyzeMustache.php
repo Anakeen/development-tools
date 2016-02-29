9691b80ff43730f3e3f68ece7e17cd62c3e2b20e
Matthieu Codron <matthieu.codron+dev@anakeen.com>
2016-02-29 15:40:04 +0100

    feat(po): extract keys from mustache templates (refs #5762)

---
+++ src/Dcp/DevTools/Po/AnalyzeMustache.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Dcp\DevTools\Po;
+
+use Dcp\DevTools\Template\Template;
+
+class AnalyzeMustache extends Analyze
+{
+    public function extract($filesPath)
+    {
+        //Remove blank elements
+        $filesPath = array_filter($filesPath, function ($value) {
+            return trim($value);
+        });
+
+        $keys = [];
+
+        foreach ($filesPath as $mustacheTpl) {
+            $extractor = new extractMustache($mustacheTpl);
+            $keys = array_merge($keys, $extractor->extractKeys());
+        }
+
+        ksort($keys);
+
+        $temporaryFile = tempnam(sys_get_temp_dir(), "mustache_keys_");
+
+        $template = new Template();
+        $template->main_render(
+            "temporary_mustache_file",
+            [
+                "keys" => array_values($keys)
+            ],
+            $temporaryFile,
+            true
+        );
+
+        parent::extract([$temporaryFile]);
+
+        unset($temporaryFile);
+    }
+} 
