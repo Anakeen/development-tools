35314f0f67aa3976a9b27c5f5a3d2782be3cb199
Aurelien Castel <aurelien.castel@anakeen.com>
2017-01-23 11:43:33 +0100

    Add parentDirPathContaining() method to the ImportFamily class
    Usefull to find the location of the import.xml file
    (root directory of the project), from the --familyPath command line argument.

--- src/Dcp/DevTools/ImportFamily/ImportFamily.php
+++ src/Dcp/DevTools/ImportFamily/ImportFamily.php
@@ -54,3 +54,32 @@ class ImportFamily
         $this->options['familyPath'] = realpath($this->options['familyPath']);
     }
 
+
+    /**
+     * Starting from $initialDirPath directory,
+     * the function goes back to the parent directory,
+     * until it reach a directory containing a file named $fileName.
+     * If it does, it returns the directory absolute path where $fileName is located.
+     * If it reach the root directory without finding $fileName it returns "".
+     *
+     * @param string $initialDirPath
+     * The directory absolute path from where to start searching for
+     * the file named $fileName.
+     * @param string $fileName The name of the file to search for.
+     * @return string
+     * The directory absolute path where $fileName is located.
+     * "" if $fileName cannot be found.
+     */
+    public function parentDirPathContaining($initialDirPath, $fileName)
+    {
+        $parentDirPath = $initialDirPath;
+        while (count(glob($parentDirPath . '/' . $fileName)) < 1) {
+            if (strlen($parentDirPath) == 1) {
+                return "";
+            }
+            $parentDirPath = dirname($parentDirPath);
+        }
+
+        return $parentDirPath;
+    }
+
